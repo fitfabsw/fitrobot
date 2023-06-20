a7c7cc36ee50f58387cfde79a949e80e416db741
Jason <u912538@gmail.com>
2023-06-20 17:52:47 +0800

    Make map thumbnail after saving map

--- fitrobot/save_map_service.py
+++ fitrobot/save_map_service.py
@@ -16,9 +16,12 @@ class MinimalService(Node):
 
     def save_map_callback(self, request, response):
         self.get_logger().info('儲存地圖到 %s' % (request.map_name))
-        stream = os.popen(f"ros2 run nav2_map_server map_saver_cli -f {MAP_FOLDER.joinpath(request.map_name)}")
-        stream.read()
-        response.ack = "SUCCESS" if not stream.close() else "FAIL"
+        map_path = MAP_FOLDER.joinpath(request.map_name)
+        r1 = os.popen(f"ros2 run nav2_map_server map_saver_cli -f {map_path}")
+        r1.read()
+        r2 = os.popen(f"convert {map_path}.pgm -resize 100x100\! {map_path}.jpg")
+        r2.read()
+        response.ack = "SUCCESS" if (not r1.close() and not r2.close()) else "FAIL"
         self.get_logger().info(f'儲存地圖結果: {response.ack}')
         
         return response
