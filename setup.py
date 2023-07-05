4a0780e0b259c450cce81bf7e44c811c3930e8ad
Jason <u912538@gmail.com>
2023-07-05 11:01:20 +0800

    Add empty launch file to control the robot bring-up

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'fitrobot'
 
@@ -7,9 +8,9 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        (f"share/{package_name}", ['package.xml']),
+        (f"share/{package_name}/launch", glob("launch/*.launch.py")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
