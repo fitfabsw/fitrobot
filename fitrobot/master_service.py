3cbcbb050dc5cc132c182f5741da8de9e0e9cf69
Jason <u912538@gmail.com>
2023-07-07 17:47:20 +0800

    Add the basic structure of the master service

--- fitrobot/master_service.py
+++ fitrobot/master_service.py
@@ -1,4 +1,8 @@
+import sys
 import rclpy
+import signal
+import psutil
+from subprocess import Popen, PIPE
 from fitrobot_interfaces.srv import Master
 from rclpy.node import Node
 
@@ -8,25 +12,57 @@ class MasterService(Node):
     def __init__(self):
         super().__init__('master_service')
         self.srv = self.create_service(Master, 'master', self.master_callback)
+        self.process_list = []
+        p = Popen(["ros2", "launch", "fitrobot", "articubot.launch.py"], stdout=PIPE, stderr=PIPE)
 
     def master_callback(self, request, response):
-        
+        request_action = request.request_action
+        if request_action == "slam":
+            self.run_slam()
+        elif request_action == "navigation":
+            self.run_navigation()
+        else:
+            pass
+
         return response
     
     def clean_up(self):
-        pass
-
+        print("清理已開啟服務")
+        while self.process_list:
+            p = self.process_list.pop()
+            print(f"清理process:{p.pid}")
+            self.kill_process_and_children(p.pid)
+            
     def run_navigation(self):
+        print("\n啟動導航服務")
         self.clean_up()
-        Node()
-        pass
+        p = Popen(["ros2", "launch", "fitrobot", "navigation.launch.py"], stdout=PIPE, stderr=PIPE)
+        self.process_list.append(p)
 
     def run_slam(self):
+        print("\n啟動建圖服務")
         self.clean_up()
-        pass
+        p = Popen(["ros2", "launch", "fitrobot", "slam.launch.py"], stdout=PIPE, stderr=PIPE)
+        self.process_list.append(p)
 
+
+    def kill_process_and_children(self, pid):
+        parent = psutil.Process(pid)
+        children = parent.children(recursive=True)
+
+        for child in children:
+            child.kill()
+
+        parent.kill()
+
+def signal_handler(signum, frame):
+    print('signal_handler: caught signal ' + str(signum))
+    if signum == signal.SIGINT.value:
+        print('SIGINT')
+        sys.exit(1)
 
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
     rclpy.init()
     master_service = MasterService()
     rclpy.spin(master_service)
--- launch/articubot.launch.py
+++ launch/articubot.launch.py
@@ -1,10 +1,25 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch import LaunchDescription, LaunchService
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     return LaunchDescription([
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(
+        #         os.path.join(
+        #             get_package_share_directory("articubot_one"),
+        #             "launch",
+        #             "launch_robot.launch.py",
+        #         )
+        #     )
+        # ),
         Node(
             package='fitrobot',
-            executable='list_map_service',
-        ),
+            executable='tf_converter_node',
+        )
     ])
