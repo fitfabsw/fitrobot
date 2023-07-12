50ded5c358dbdd2646747453b96c3ec86ce1ada1
Jason <u912538@gmail.com>
2023-07-12 08:46:08 +0800

    Add utils.py and put get_logger function in it

---
+++ common/utils.py
@@ -0,0 +1,20 @@
+import logging
+
+def get_logger(logger_name):
+    # create logger
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('[%(name)s] %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
