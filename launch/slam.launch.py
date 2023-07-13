84ee9953f924291d55756a7872f4b629b56f271c
Jason <u912538@gmail.com>
2023-07-13 15:34:00 +0800

    Add the actual slam launch file to slam.launch.py

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -1,11 +1,19 @@
+import os
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node(
-        #     package='fitrobot',
-        #     executable='list_map_service',
-        # ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory("slam_toolbox"),
+                    "launch",
+                    "online_async_launch.py",
+                )
+            )
+        ),
     ])
