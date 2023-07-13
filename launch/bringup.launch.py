519d5450cd453ac187c17dd0bf2935407c6a1946
Jason <u912538@gmail.com>
2023-07-13 15:32:00 +0800

    Uncomment the articubot one launch code block

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -9,15 +9,15 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     return LaunchDescription([
-        # IncludeLaunchDescription(
-        #     PythonLaunchDescriptionSource(
-        #         os.path.join(
-        #             get_package_share_directory("articubot_one"),
-        #             "launch",
-        #             "launch_robot.launch.py",
-        #         )
-        #     )
-        # ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory("articubot_one"),
+                    "launch",
+                    "launch_robot.launch.py",
+                )
+            )
+        ),
         IncludeLaunchDescription(
             FrontendLaunchDescriptionSource(
                 os.path.join(
