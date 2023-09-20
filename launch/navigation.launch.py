7d89b94bf5d63b24842863fc93ed88efaaefdcc9
Jason <u912538@gmail.com>
2023-09-20 09:50:55 +0800

    Use param file with MPPI controller by default

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -19,6 +19,6 @@ def generate_launch_description():
                     "bringup_launch.py",
                 ),
             ),
-            launch_arguments = {"map": map_lc, "params_file": "/home/pi/zbot_ws/src/articubot_one/config/nav2_params_20230717.yaml"}.items()
+            launch_arguments = {"map": map_lc, "params_file": "/home/pi/zbotartic_ws/src/articubot_one/config/nav2_params.yaml"}.items()
         ),
     ])
