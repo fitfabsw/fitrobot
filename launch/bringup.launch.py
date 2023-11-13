57e1529690d3384b0959fe4807aca615a35b626e
zealzel <zealzel@gmail.com>
2023-11-13 08:40:39 +0800

    fitrobotcpp updated. Set fitrobotcpp's check_robot_status as default again in bringup.py.

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -34,14 +34,14 @@ def generate_launch_description():
             package='fitrobot',
             executable='list_map_service',
         ),
-        Node(
-            package='fitrobot',
-            executable='check_robot_status',
-        ),
         # Node(
-        #     package='fitrobotcpp',
+        #     package='fitrobot',
         #     executable='check_robot_status',
         # ),
+        Node(
+            package='fitrobotcpp',
+            executable='check_robot_status',
+        ),
         Node(
             package='fitrobot',
             executable='waypoint_follower',
